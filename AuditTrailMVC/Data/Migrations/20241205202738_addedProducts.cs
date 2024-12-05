3c2f3ea0f3a41096ee7e0a59abf4d8a069fa13b8
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-12-05 22:30:56 +0200

    Audit Trail Implementation in ASP.NET Core with Entity Framework Core

---
+++ AuditTrailMVC/Data/Migrations/20241205202738_addedProducts.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuditTrailMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ AuditTrailMVC/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AuditTrailMVC.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
